20869524bc26d321190ca47ca0cffd3aee7c8580
Dusko Vokic <vokic.dusko@gmail.com>
2025-11-08 22:42:21 +0100

    Initial commit - Tic Tac Toe React project

---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import "./Player.css";
+
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing); // menja inicijalno stanje sa false na true
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {!isEditing && <span className="player-name">{playerName}</span>}
+        {isEditing && (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
+        )}
+
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+// import ReactDOM from "react-dom/client";
+
+// import App from "./App.jsx";
+// import "./index.css";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+// const entryPoint = document.getElementById("root");
+// ReactDOM.createRoot(entryPoint).render(<App />);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
+
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(<App />);
